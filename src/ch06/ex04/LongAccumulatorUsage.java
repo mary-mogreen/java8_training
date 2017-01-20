f86d12b4f4bf3c91fc019ac2ce0ee94d9ea749b9
mary-mogreen <oyu.javajava@gmail.com>
2017-01-20 15:05:00 +0900

    渡されたリストが空の場合に例外を投げるように変更。

--- src/ch06/ex04/LongAccumulatorUsage.java
+++ src/ch06/ex04/LongAccumulatorUsage.java
@@ -10,6 +10,10 @@ import java.util.concurrent.atomic.LongAccumulator;
 public class LongAccumulatorUsage {
     public static long max(List<Long> longs) {
         Objects.requireNonNull(longs, "long value list is null.");
+        
+        if (longs.isEmpty())
+            throw new IllegalArgumentExceotion("list is empty.");
+        
         LongAccumulator max = new LongAccumulator((l1, l2) -> {
             return l1 > l2 ? l1: l2;
         }, Long.MIN_VALUE);
@@ -19,6 +23,10 @@ public class LongAccumulatorUsage {
 
     public static long min(List<Long> longs) {
         Objects.requireNonNull(longs, "long value list is null.");
+        
+        if (longs.isEmpty())
+            throw new IllegalArgumentExceotion("list is empty.");
+        
         LongAccumulator min = new LongAccumulator((l1, l2) -> {
             return l1 > l2 ? l2: l1;
         }, Long.MAX_VALUE);
